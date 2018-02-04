2963b632454bd01aa34bd55cb74c846cac7b6313
Ángel García <angel.garcia.ins@gmail.com>
2018-02-04 11:34:06 +0100

    Initial commit

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
